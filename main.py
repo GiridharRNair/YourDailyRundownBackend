7a40d3d91f364010110c0b3106507319d3552103
Giridhar Nair <Giridhar.r.nair@gmail.com>
2023-08-31 13:50:43 -0500

    Transferred to MongoDB

--- main.py
+++ main.py
@@ -1,174 +1,149 @@
-from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
-import sqlite3
 import os
+from flask_cors import CORS
 from dotenv import load_dotenv
+from pymongo import MongoClient
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from flask import Flask, request, jsonify, render_template
 
-# Load environment variables
-project_folder = os.path.expanduser('/home/GiridharNair/mysite')
-load_dotenv(os.path.join(project_folder, '.env'))
-
-app = Flask(__name__, template_folder="/home/GiridharNair/mysite/templates")
-CORS(app)  # Allow cross-origin requests (for development)
-DB_NAME = "/home/GiridharNair/mysite/users.db"
-
-
-def create_table():
-    """Create the users table in the database if it doesn't exist."""
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY,
-            first_name TEXT NOT NULL,
-            last_name TEXT NOT NULL,
-            email TEXT NOT NULL UNIQUE,
-            category TEXT NOT NULL
-        )
-    """)
-    conn.commit()
-    conn.close()
+load_dotenv()
+client = MongoClient(os.environ.get('MONGO_URI'))
+users_collection = client.users["users"]
+app = Flask(__name__, template_folder="templates")
+CORS(app)
+category_mapping = {
+    "Realestate": "Real Estate",
+    "Nyregion": "NY Region",
+    "Us": "U.S."
+}
 
 
 @app.route("/register_user", methods=["POST"])
 def add_user():
-    """Add a new user to the database and send a greeting email."""
-    data = request.json
-    first_name = data.get("firstName")
-    last_name = data.get("lastName")
-    email = data.get("email").lower()
-    categories = ','.join(item['value'] for item in data.get("category"))
-    conn = sqlite3.connect(DB_NAME)
+    """
+    Registers a new user by extracting user data from the request JSON.
+    Inserts the user's information into the MongoDB database and sends
+    a welcome email to the user.
+
+    Returns:
+        JSON response indicating the success or failure of the operation.
+    """
     try:
-        add_user_to_database(conn, email, first_name, last_name, categories)
+        data = request.json
+        first_name = data["firstName"]
+        last_name = data["lastName"]
+        email = data["email"].lower()
+        categories = [item['value'] for item in data["category"]]
+        add_user_to_database(email, first_name, last_name, categories)
         send_greeting_email(email, first_name, last_name, categories)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    finally:
-        conn.close()
-
     return jsonify({"message": "User added successfully"}), 201
 
 
 @app.route('/registered_users', methods=['GET'])
 def get_users():
-    """Retrieve a list of all registered users from the database."""
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    cursor.execute("""
-        SELECT *
-        FROM users
-    """)
-    rows = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return rows
+    """
+    Retrieves a list of registered users from the MongoDB database and
+    returns it as a JSON response.
+
+    Returns:
+    JSON response containing a list of registered user information.
+    """
+    user_list = [
+        {
+            'id': str(user['_id']),
+            'first_name': user['first_name'],
+            'last_name': user['last_name'],
+            'email': user['email'],
+            'category': user['category']
+        }
+        for user in users_collection.find()
+    ]
+    return jsonify(user_list)
 
 
 @app.route('/<email>/unsubscribe')
 def unsubscribe(email):
-    """Unsubscribe a user from the newsletter and render the unsubscribe page."""
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
+    """
+    Handles the unsubscribe process for a user with the provided email address.
+    Deletes the user's information from the MongoDB database and renders an
+    unsubscribe confirmation page.
 
-    cursor.execute("""
-    SELECT first_name, last_name
-    FROM users
-    WHERE email = ?;
-    """, (email.lower(),))
+    Args:
+        email (str): The email address of the user to unsubscribe.
 
-    result = cursor.fetchone()
-    if result:
-        first_name, last_name = result
-
-        cursor.execute("""
-        DELETE FROM users
-        WHERE email = ?;
-        """, (email.lower(),))
-
-        conn.commit()
-        cursor.close()
-        conn.close()
-
+    Returns:
+        A rendered template for the unsubscribe confirmation page.
+    """
+    user = users_collection.find_one_and_delete({'email': email.lower()})
+    if user:
+        first_name = user['first_name']
+        last_name = user['last_name']
         return render_template('unsubscribe_page.html', first_name=first_name, last_name=last_name)
     else:
-        cursor.close()
-        conn.close()
         return render_template('user_not_found_page.html')
 
 
 def send_greeting_email(email, first_name, last_name, categories):
-    """Send a welcome email to a newly registered user."""
+    """
+    Sends a welcome email to a newly registered user.
+
+    Args:
+        email (str): The email address of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+        categories (list): A list of user-selected categories.
+
+    Note:
+        This function uses the SendGrid API to send emails.
+
+    Returns:
+        None
+    """
     try:
-        categories_list = categories.split(',')
-
-        modified_categories = []
-        for category in categories_list:
-            if category.lower() == "realestate":
-                modified_categories.append("Real Estate")
-            elif category.lower() == "nyregion":
-                modified_categories.append("NY Region")
-            elif category.lower() == "us":
-                modified_categories.append("U.S.")
-            else:
-                modified_categories.append(category)
-
-        if len(modified_categories) > 1:
-            user_categories = ', '.join(modified_categories[:-1]) + ', and ' + modified_categories[-1]
-        else:
-            user_categories = modified_categories[0]
-
-        greeting_email = Mail(
+        formatted_categories = [category.title() for category in categories]
+        formatted_categories = [category_mapping.get(category, category) for category in formatted_categories]
+        user_categories = ', '.join(formatted_categories[:-1]) + ', and ' + \
+                          formatted_categories[-1] if len(formatted_categories) > 1 else formatted_categories[0]
+        with open('templates/registration_email.txt', 'r') as file:
+            registration_email_content = file.read()
+        registration_email_content = registration_email_content.format(first_name, last_name, user_categories, email)
+        registration_email = Mail(
             from_email='yourdailyrundown@gmail.com',
             to_emails=email,
             subject='Your Daily Rundown - Welcome!',
-            html_content=(
-                f"<p>Hey {first_name} {last_name},</p>\n\n"
-                f"<h2>Thanks for joining YourDailyRundown</h2>\n\n"
-                f"<p>Welcome to our newsletter! We're thrilled to have you on board.</p>\n\n"
-                f"<p>With our newsletter, you'll stay informed about the latest news and updates, all carefully "
-                f"curated and summarized to match your interest(s) in {user_categories}.</p>\n\n"
-                f"<p>Want to personalize your experience even further? You can easily update your preferences and "
-                f"name by re-registering for our newsletter. Don't worry about duplicate emails – we've got that "
-                f"covered, and all your changes will be recorded seamlessly.</p>\n\n"
-                f"<p>If, at any point, you wish to stop receiving our updates, you can simply click the <a "
-                f"href='https://giridharnair.pythonanywhere.com/{email}/unsubscribe'>unsubscribe</a> link provided at "
-                f"the bottom of"
-                f"each email.</p>\n\n"
-                f"<p>Best regards,<br />Giridhar Nair<br />YourDailyRundown</p>"
-            )
+            html_content=registration_email_content
         )
-        response = SendGridAPIClient(os.getenv('SENDGRID_API_KEY')).send(greeting_email)
-        print(f"Email sent to {email}, status code: {response.status_code}")
+        SendGridAPIClient(os.getenv('SENDGRID_API_KEY')).send(registration_email)
     except Exception as e:
         print(f"Failed to send email to {email}. Error: {str(e)}")
-        raise
 
 
-def add_user_to_database(conn, email, first_name, last_name, categories):
-    """Add a user to the database or update their details if they already exist."""
-    try:
-        with conn:
-            cursor = conn.cursor()
-            cursor.execute("""
-                INSERT OR IGNORE INTO users (email, first_name, last_name, category)
-                VALUES (?, ?, ?, ?)
-            """, (email, first_name, last_name, categories))
+def add_user_to_database(email, first_name, last_name, categories):
+    """
+    Adds a user's information to the MongoDB database.
 
-            cursor.execute("""
-                UPDATE users
-                SET category = ?,
-                    first_name = ?,
-                    last_name = ?
-                WHERE email = ?
-            """, (categories, first_name, last_name, email))
-    except sqlite3.Error as e:
-        print(f"Database error: {str(e)}")
-        raise
+    Args:
+        email (str): The email address of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+        categories (list): A list of user-selected categories.
+
+    Returns:
+        None
+    """
+    user_query = {'email': email}
+    user_update = {
+        '$set': {
+            'email': email,
+            'first_name': first_name,
+            'last_name': last_name,
+            'category': categories
+        }
+    }
+    users_collection.update_one(user_query, user_update, upsert=True)
 
 
 if __name__ == "__main__":
-    create_table()
-    app.run()  # Start the Flask application
+    app.run(debug=True)
